5a645d3436d792dc9b6e3e20a7cb81f680fb035e
Nachaor12 <163567793+Nachaor12@users.noreply.github.com>
2025-07-10 16:05:04 -0400

    CardFormat cambiado a Init para manejo en las demás paginas

--- lib/pages/init.dart
+++ lib/pages/init.dart
@@ -5,6 +5,26 @@ import 'package:my_steam_news/pages/others.dart';
 import 'package:my_steam_news/pages/preferences.dart';
 import 'package:my_steam_news/pages/search.dart';
 import 'package:my_steam_news/pages/userprofile.dart';
+import 'package:my_steam_news/domain/entities/noticia.dart';
+import 'package:my_steam_news/domain/entities/usuario.dart';
+import 'package:my_steam_news/data/services/service_news.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+
+//Link para detalles del juego: https://store.steampowered.com/api/appdetails?appids=2651280
+//Link para juegos del usuario: http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=DAF9764CEB1934D64B009F26CF5F8F63&steamid=*iduser*&format=json
+//Link para las noticias de juegos: http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=440&count=3&maxlength=300&format=json
+
+//Frano ID = 76561198071742485
+//Nachoar ID = 76561199176277858
+
+Color colorCards = Colors.blue;
+TextStyle cardTitleTextStyle = TextStyle(fontSize: 30);
+int quantityOfNewsPerGame = 3;
+
+//Lista de noticias totales de los juegos del usuario
+List<Noticia> listGameHome = [];
 
 
 // Pagina Home de la aplicación, la que aparece al ingresar
@@ -17,12 +37,63 @@ class InitPage extends StatefulWidget {
 }
 
 class _InitPageState extends State<InitPage> {
-
   int currentPageIndex = 0;
+
+  ServiceNews serviceNew = ServiceNews();
+
+  //User? userprof;
+  User userprof = User(id: 76561199176277858, idsGames: [], gameCount: 0);
+  //Lista de noticias por juego
+  List<Noticia> listGamesUserNewsPerGame = [];
 
   @override
   void initState(){
     super.initState();
+
+    //Conseguir IDs del usuario
+    serviceNew
+      .getUserGamesIds(userprof.id.toString())
+      .then((user) {
+        setState(() {
+          userprof = user;
+        });
+
+        showIdHalfLife(userprof);
+
+        if (userprof.idsGames.isNotEmpty) {
+          for (var idGame in userprof.idsGames) {
+            serviceNew
+                .getNews(idGame.toString(), quantityOfNewsPerGame.toString())
+                .then((news) {
+                  setState(() {
+                    listGamesUserNewsPerGame = news;
+                    print('${listGamesUserNewsPerGame.length}');
+                    for (var gameNews in listGamesUserNewsPerGame) {
+                      listGameHome.add(gameNews);
+                    }
+                  });
+                });
+          }
+        }
+      })
+      .catchError((e) => print('failed to work with API data $e'));
+    /*listGameHome = [
+      Noticia(
+        gid: '70',
+        title: 'Half-Life 3 confirmado',
+        date: '2025-07-10',
+        feedLabel: 'Valve News',
+        url: 'https://store.steampowered.com/app/70',
+      ),
+      Noticia(
+        gid: '400',
+        title: 'Portal 3 en desarrollo',
+        date: '2025-07-09',
+        feedLabel: 'GabeN Oficial',
+        url: 'https://store.steampowered.com/app/400',
+      ),
+    ];*/
+    print('Se cargaron los datos de las API');
   }
 
   @override
@@ -73,7 +144,7 @@ class _InitPageState extends State<InitPage> {
       ),
       // Body para agregar los Widget del BottomNavigationBar
       body: <Widget> [
-        Homepage(),
+        Homepage(listGameNewsHome: listGameHome, newsFormat: cardFormat,),
         SearchPage(),
         FavoritePage(),
         OtherPage()
@@ -81,7 +152,6 @@ class _InitPageState extends State<InitPage> {
     );
   }
 }
-
 
 // Funciones para cambiar entre paginas (Navegación)
 // Para ir a la pagina del perfil de usuario
@@ -93,4 +163,58 @@ void changeToUserPage(BuildContext context){
 void changeToPreferencesPage(BuildContext context){
   print('Cambiamos a la pagina de preferencias');
   Navigator.push(context, MaterialPageRoute(builder: (context) => PreferencesPage()));
+}
+
+//Formato de Card para mostrar las noticias
+//Formato de la Card que muestra la noticia
+Widget cardFormat(Noticia noticia) {
+  print('Se deberian ver algunas noticias, las ves tú?');
+  return Card(
+    elevation: 4,
+    color: colorCards,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+    child: InkWell(
+      onTap: () {
+        _launchUrl(noticia.url); //con url_launcher
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              noticia.feedLabel,
+              style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 14),
+            ),
+            const SizedBox(height: 6),
+            Text(noticia.title, style: cardTitleTextStyle),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                Text(
+                  noticia.date,
+                  style: const TextStyle(fontSize: 12, color: Colors.white70),
+                ),
+                const Spacer(),
+                IconButton(
+                  onPressed: () {
+                    // Compartir noticia
+                  },
+                  icon: const Icon(Icons.share, size: 20),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+Future<void> _launchUrl(String _url) async {
+  final Uri _url1 = Uri.parse(_url);
+
+  if (!await launchUrl(_url1)) {
+    throw Exception('Could not launch $_url');
+  }
 }
